ab12afd3ffa602af7a7679dff952107d47e94087
ade-sede <adrien.de.sede@gmail.com>
2017-05-16 13:53:41 +0200

    Checked path recovery. Its working

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ade-sede <adrien.de.sede@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 14:38:25 by ade-sede          #+#    #+#             */
-/*   Updated: 2017/05/15 22:54:25 by ade-sede         ###   ########.fr       */
+/*   Updated: 2017/05/16 12:41:14 by ade-sede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,21 @@ int	main(void)
 {
 	t_env			env;
 	extern const char	**environ;
-	t_list		*first;
+	/* t_list		*first; */
 
 	load_base_env(&env, environ);
-	print_environ((const char **)env.environ);
-	first = env.path;
-	while (first)
-	{
-		ft_printf("%s\n", first->data);
-		first = first->next;
-	}
+	/* print_environ((const char **)env.environ); */
+	/* first = env.path; */
+	/* while (first) */
+	/* { */
+	/* 	ft_printf("%s\n", first->data); */
+	/* 	first = first->next; */
+	/* } */
+
+
+
+	char	*str[] = 	{"/bin/echo", "lol", NULL };
+	execve(str[0], str, env.environ);
+
 	return (0);
 }
