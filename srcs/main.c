85f10d17103ca06b830ce3c0f9fe31c04580e016
ade-sede <adrien.de.sede@gmail.com>
2017-05-17 15:11:59 +0200

    Fixed the leak in remove_char

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ade-sede <adrien.de.sede@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 14:38:25 by ade-sede          #+#    #+#             */
-/*   Updated: 2017/05/16 12:41:14 by ade-sede         ###   ########.fr       */
+/*   Updated: 2017/05/17 14:59:45 by ade-sede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,21 @@ int	main(void)
 	/* t_list		*first; */
 
 	load_base_env(&env, environ);
+	print_environ((const char **)env.environ);
+
+	/* size_t index; */
+
+	/* ft_get_env((const char**)env.environ, "SHLVL", &index); */
+	/* env.environ = (char**)ft_freeswap((t_ulong)env.environ, (t_ulong)remove_var(env.environ, env.environ_size, index)); */
 	/* print_environ((const char **)env.environ); */
+
 	/* first = env.path; */
 	/* while (first) */
 	/* { */
 	/* 	ft_printf("%s\n", first->data); */
 	/* 	first = first->next; */
 	/* } */
-
-
-
-	char	*str[] = 	{"/bin/echo", "lol", NULL };
-	execve(str[0], str, env.environ);
-
 	return (0);
 }
+
+
--- srcs/unsetenv.c
+++ srcs/unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: ade-sede <adrien.de.sede@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/15 14:45:01 by ade-sede          #+#    #+#             */
-/*   Updated: 2017/05/15 17:49:30 by ade-sede         ###   ########.fr       */
+/*   Updated: 2017/05/17 15:08:37 by ade-sede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ char		**remove_var(char **environ, size_t environ_size, size_t key_index)
 	char	**new_environ;
 
 	new_environ = palloc(sizeof(char*) * (environ_size - 1 + 1), __func__);
+	free(environ[key_index]);
 	ft_addcpy((void**)new_environ, (const void**)environ, key_index);
 	ft_addcpy((void**)new_environ + key_index, \
 			(const void**)environ + key_index + 1, environ_size - key_index);
