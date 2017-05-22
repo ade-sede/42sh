4e5179e84fad457f4fd40f59812c67e0b3e4a8f4
ade-sede <adrien.de.sede@gmail.com>
2017-05-22 11:36:00 +0200

    The start of expand_arg fcts

---
+++ srcs/exec/expand_args.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_args.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-sede <adrien.de.sede@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/21 14:38:59 by ade-sede          #+#    #+#             */
+/*   Updated: 2017/05/21 14:57:54 by ade-sede         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "exec.h"
+
+short int	dollar_sign_is_valid(const char *argv, size_t i)
+{
+	short int	valid;
+	if 
+}
+
+void		exec_expand_args(t_env *env, char **argv)
+{
+	int	i;
+	short int	dollar_sign;
+
+	dollar_sign = false;
+	i = 0;
+	while (*argv != NULL)
+	{
+		while (*argv[i])
+		{
+		}
+		argv++;
+	}
+}
