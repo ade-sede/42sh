fad8be43e78d4c85d89315251712e4ffcc87616c
vcombey <vcombey@student.42.fr>
2017-05-23 14:56:31 +0200

    completion ajoute un mot au milieu de la ligne avec fonction addstrbuffline

--- srcs/completion/comple_exit_matched.c
+++ srcs/completion/comple_exit_matched.c
@@ -16,6 +16,18 @@ void	comple_free(t_comple c)
 	ft_arraydel(&c.matches);
 }
 
+int	insert_str_linebuff(char *str, char *dest)
+{
+	size_t	len;
+
+	len = ft_strlen(str);
+	ft_memmove((void*)(dest + len), (void*)(dest), ft_strlen(dest));
+	//memset(dest, 48, ft_strlen(dest));
+	//ft_putendl(dest);
+	ft_strncpy(dest, str, len);
+	return (1);
+}
+
 int	comple_exit_matched(t_line *line, t_comple c)
 {
 	size_t	selected_len;
@@ -23,13 +35,10 @@ int	comple_exit_matched(t_line *line, t_comple c)
 	
 	selected_str = c.matches[c.pos];
 	selected_len = ft_strlen(selected_str);
-	if (line->pos == line->len - 1)
+	if (line->pos == line->len)
 		ft_strcpy(line->buff + line->len, selected_str);
 	else
-	{
-		ft_memmove((void*)(line->buff + line->pos + selected_len), (void*)(line->buff + line->pos), line->len - line->pos);
-		ft_strcpy(line->buff + line->pos, selected_str);
-	}
+		insert_str_linebuff(selected_str, line->buff + line->pos);
 	line->pos += selected_len;
 	line->len += selected_len;
 	comple_clear(c);
