5032fa7697f0b864ae78b9fba67e6204fde5724c
ade-sede <adrien.de.sede@gmail.com>
2017-05-23 12:46:10 +0200

    Echo : added backslash interpretation and unicode interpretation with flag -e

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -2,34 +2,100 @@
 #include "env.h"
 #include <stdio.h>
 
-static const char **parse_options(const char **argv, short int *n_flag)
+static const char **parse_options(const char **argv, short int *n_flag, short int *e_flag)
 {
-	char	*option_array[2];
+	char	*option_array[5];
 
 	option_array[0] = "-n";
-	option_array[1] = NULL;
+	option_array[1] = "-e";
+	option_array[2] = "-ne";
+	option_array[3] = "-en";
+	option_array[4] = NULL;
 	while (ft_arraychr((const char**)option_array, (const char*)*argv))
 	{
 		if (ft_strequ(*argv, "-n"))
 			*n_flag = true;
+		if (ft_strequ(*argv, "-e"))
+			*e_flag = true;
+		if (ft_strequ(*argv, "-ne") || ft_strequ(*argv, "-en"))
+		{
+			*e_flag = true;
+			*n_flag = true;
+		}
 		argv++;
 	}
 	return (argv);
 }
 
+static void		print_unicode(const char *argv, size_t *i)
+{
+	size_t	n;
+	size_t	start;
+	size_t	end;
+	char		*unicode_char;
+	char		*tmp;
+
+	n = 4;
+	start = *i;
+	end = *i;
+	while (n && ft_ishex(argv[end]))
+	{
+		n--;
+		end++;
+	}
+	tmp = ft_strsub(argv, start, end - start);
+	unicode_char = ft_buff_wchar((wchar_t)ft_atoi_base(tmp, 16));
+	ft_putstr(unicode_char);
+	free(unicode_char);
+	free(tmp);
+	*i = end - 1;
+}
+
+static void		print_arg(const char *argv)
+{
+	size_t	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (charcmp(argv, i, '\\'))
+		{
+			i++;
+			if (argv[i] == 'n')
+				ft_putchar('\n');
+			if (argv[i] == 't')
+				ft_putchar('\t');
+			if (argv[i] == 'v')
+				ft_putchar('\v');
+			if (argv[i] == 'u')
+			{
+				i++;
+				print_unicode(argv, &i);
+			}
+		}
+		else
+			ft_putchar(argv[i]);
+		i++;
+	}
+}
 
 int	builtin_echo(t_env *env, const char **argv)
 {
 	short int n_flag;
+	short int e_flag;
 	const char	**tmp;
 
 	(void)env;
 	n_flag = false;
-	argv = parse_options(argv + 1, &n_flag);
+	e_flag = false;
+	argv = parse_options(argv + 1, &n_flag, &e_flag);
 	tmp = argv;
 	while (*argv)
 	{
-		ft_putstr(*argv);
+		if (!e_flag)
+			ft_putstr(*argv);
+		else
+			print_arg(*argv);
 		if (*(argv + 1))
 			ft_putchar(32);
 		argv++;
