f1cbffa7e21e57bad1de5d3aed44ca6819f3afb8
vcombey <vcombey@student.42.fr>
2017-05-24 18:40:36 +0200

    refresh multiligne !

--- srcs/line_editing/edit_refresh.c
+++ srcs/line_editing/edit_refresh.c
@@ -9,16 +9,23 @@ void	edit_refresh_cursor(t_line *line)
 	size_t	ws_col;
 
 	ws_col = line->ws_col;
-	src_col = (line->len + 2) % ws_col;
-	dest_col = (line->pos + 2) % ws_col;
-	src_line = (line->len + 2) / ws_col;
-	dest_line = (line->pos + 2) / ws_col;
+	src_col = (line->len + 2) % (ws_col - 1);
+	dest_col = (line->pos + 2) % (ws_col - 1);
+	src_line = (line->len + 2) / (ws_col - 1);
+	dest_line = (line->pos + 2) / (ws_col - 1);
 	put_ntermcap("up", src_line - dest_line);
 	if (dest_col > src_col)
-		put_ntermcap("ri", dest_col - src_col);
+	{
+		put_ntermcap("nd", dest_col - src_col);
+		//ft_putnbr(dest_col - src_col);
+	}
 	else 
+	{
 		put_ntermcap("le", src_col - dest_col);
+		//ft_putnbr(src_col - dest_col);
+	}
 }
+
 /*
 ** refresh line write buff on multiple lines per ws_col.
 */
@@ -34,9 +41,14 @@ void	edit_refresh_line(t_line *line)
 	while (i < line->len)
 	{
 		put_termcap("do");
+		put_termcap("cr");
 		ft_putnstr(singleton_line()->buff + i, ws_col - 1);
 		i += ws_col - 1;
 	}
+	if ((line->len + 2) % (ws_col - 1) == 0)
+		put_termcap("do");
+
+	//put_termcap("le");
 	//put_termcap("up");
 }
 
@@ -44,10 +56,12 @@ void	edit_refresh_clear(t_line *line)
 {
 	size_t	init_line;
 
-	init_line = (line->pos + 2) / line->ws_col;
+	init_line = (line->old_pos + 2) / (line->ws_col - 1);
 	put_ntermcap("up", init_line);
 	put_termcap("cr");
+	//put_ntermcap("dl", init_line);
 	put_termcap("cd");
+	//put_termcap("do");
 }
 
 void	edit_refresh(int signum, t_env *env)
