c537c6421772cd0188026e4037d01a3e2e56cba9
vcombey <vcombey@student.42.fr>
2017-05-25 18:29:09 +0200

    protection du copier et du coller

--- srcs/line_editing/copy_paste.c
+++ srcs/line_editing/copy_paste.c
@@ -9,16 +9,20 @@ int	enter_visual_mode(t_line *line)
 
 int	copy(t_line *line)
 {
-	if (line->visu_start < line->pos)
-		line->copied_text = ft_strndup(line->buff + line->visu_start, line->pos - line->visu_start);
-	else if (line->visu_start > line->pos)
-		line->copied_text = ft_strndup(line->buff + line->pos, line->visu_start - line->pos);
-	line->visu_mode = 0;
+	if (line->visu_mode)
+	{
+		if (line->visu_start < line->pos)
+			line->copied_text = ft_strndup(line->buff + line->visu_start, line->pos - line->visu_start);
+		else if (line->visu_start > line->pos)
+			line->copied_text = ft_strndup(line->buff + line->pos, line->visu_start - line->pos);
+		line->visu_mode = 0;
+	}
 	return (1);
 }
 
 int	paste(t_line *line)
 {
-	edit_insert_str(line, line->copied_text);
+	if (line->copied_text)
+		edit_insert_str(line, line->copied_text);
 	return (1);
 }
