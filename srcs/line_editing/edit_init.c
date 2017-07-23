d218ccd7b01842a1cf8d031719a46bbc7b417e38
vcombey <vcombey@student.42.fr>
2017-07-23 17:17:41 +0200

    remalloc a buff_line_size dans edit line init

--- srcs/line_editing/edit_init.c
+++ srcs/line_editing/edit_init.c
@@ -6,7 +6,7 @@
 /*   By: vcombey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/08 23:19:54 by vcombey           #+#    #+#             */
-/*   Updated: 2017/06/08 23:20:19 by vcombey          ###   ########.fr       */
+/*   Updated: 2017/07/23 17:16:49 by vcombey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ t_line		*singleton_line(void)
 void		edit_line_init(t_line *line)
 {
 	ft_bzero(line->buff, line->len);
+	if (line->size > BUFF_LINE_SIZE)
+	{
+	    free(line->buff);
+	    line->buff = ft_strnew(BUFF_LINE_SIZE);
+	    line->size = BUFF_LINE_SIZE;
+	}
 	line->visu_mode = 0;
 	line->pos = 0;
 	line->len = 0;
