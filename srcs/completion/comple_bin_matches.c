bb7671348744333adc72dc082990cab97d89c7db
vcombey <vcombey@student.42.fr>
2017-08-24 00:17:46 +0200

    en cours

--- srcs/completion/comple_bin_matches.c
+++ srcs/completion/comple_bin_matches.c
@@ -1,19 +1,21 @@
 #include "env.h"
 #include "completion.h"
 
-void	comple_bin_matches_tripping(t_ternary_tree *node, char **matches)
+void	comple_bin_matches_tripping_tree(t_ternary_tree *node, char **matches, to_matchs)
 {
+	if (!cur[0]) 
+		comple_bin_matches_extract_tree(node, matches);
 	if (*cur < node->c && (!node->left || (node->left && *cur > node->left->c)))// aret
-		return ternary_tree_insert_left(node, cur, str);
+		return matches;
 	else if (*cur > node->c && (!node->right || (node->right && *cur < node->right->c))) // aret
-		return ternary_tree_insert_right(node, cur, str);
+		return matches;
 	else if (*cur < node->c)
-		return ternary_tree_add(node->left, cur, str);
+		return comple_bin_matches_tripping_tree(node->left, matches);
 	else if (*cur > node->c)
-		return ternary_tree_add(node->right, cur, str);
+		return comple_bin_matches_tripping_tree(node->right, matches);
 	else if (*cur == node->c && node->down)
 		return ternary_tree_add(node->down, cur + 1, str);
-	else if (*cur == node->c && node->down)
+	else if (*cur == node->c && !node->down)
 		return ternary_tree_insert_down(node, cur, str);
 }
 
--- srcs/completion/ternary_search_tree.c
+++ srcs/completion/ternary_search_tree.c
@@ -6,60 +6,28 @@
 **	
 */
 
+void	ternary_tree_add(t_ternary_tree *node, char *cur, char *str)
+{
+	if (*cur < node->c && (!node->left || (node->left && *cur > node->left->c)))// aret
+		return ternary_tree_insert_left(node, cur, str);
+	else if (*cur > node->c && (!node->right || (node->right && *cur < node->right->c))) // aret
+		return ternary_tree_insert_right(node, cur, str);
+	else if (*cur < node->c)
+		return ternary_tree_add(node->left, cur, str);
+	else if (*cur > node->c)
+		return ternary_tree_add(node->right, cur, str);
+	else if (*cur == node->c && node->down)
+		return ternary_tree_add(node->down, cur + 1, str);
+	else if (*cur == node->c && !node->down)
+		return ternary_tree_insert_down(node, cur, str);
+}
+
 
 /*
 **	
 **	insere le nouveau neud new_down a gauche de node comme dans un liste doublement chaine 
 **	
 */
-
-t_ternary_tree		*ternary_tree_new(char c)
-{
-	t_ternary_tree *new;
-
-	if (!(new = ft_memalloc(sizeof(t_ternary_tree))))
-		exit(1);
-	new->c = c;
-	return (new);
-}
-
-void			ternary_tree_print(t_ternary_tree *node)
-{
-	if (!node)
-		return ;
-	printf("c is %c\n", node->c);
-	if (node->match)
-		printf("match is %s\n", node->match);
-	if (node->left)
-		printf("\n left \n");
-	ternary_tree_print(node->left);
-	if (node->down)
-		printf("\n down \n");
-	ternary_tree_print(node->down);
-	//put_termcap("do");
-	if (node->right)
-		printf("\n right\n");
-	ternary_tree_print(node->right);
-}
-
-t_ternary_tree		*ternary_tree_new_down(char *str, char *cur)
-{
-	t_ternary_tree *res;
-	t_ternary_tree *tmp;
-
-	res = ternary_tree_new(*cur);
-	tmp = res;
-	cur++;
-	while (*cur)
-	{
-		tmp->down = ternary_tree_new(*cur);
-		if (*cur)
-			tmp = tmp->down;
-		cur++;
-	}
-	tmp->match = ft_strdup(str);
-	return (res);
-}
 
 
 void	ternary_tree_insert_left(t_ternary_tree *node, char *cur, char *str)
@@ -95,20 +63,53 @@ void	ternary_tree_insert_down(t_ternary_tree *node, char *cur, char *str)
 }
 
 
-void	ternary_tree_add(t_ternary_tree *node, char *cur, char *str)
+t_ternary_tree		*ternary_tree_new_down(char *str, char *cur)
 {
-	if (*cur < node->c && (!node->left || (node->left && *cur > node->left->c)))// aret
-		return ternary_tree_insert_left(node, cur, str);
-	else if (*cur > node->c && (!node->right || (node->right && *cur < node->right->c))) // aret
-		return ternary_tree_insert_right(node, cur, str);
-	else if (*cur < node->c)
-		return ternary_tree_add(node->left, cur, str);
-	else if (*cur > node->c)
-		return ternary_tree_add(node->right, cur, str);
-	else if (*cur == node->c && node->down)
-		return ternary_tree_add(node->down, cur + 1, str);
-	else if (*cur == node->c && node->down)
-		return ternary_tree_insert_down(node, cur, str);
+	t_ternary_tree *res;
+	t_ternary_tree *tmp;
+
+	res = ternary_tree_new(*cur);
+	tmp = res;
+	cur++;
+	while (*cur)
+	{
+		tmp->down = ternary_tree_new(*cur);
+		if (*cur)
+			tmp = tmp->down;
+		cur++;
+	}
+	tmp->match = ft_strdup(str);
+	return (res);
+}
+
+
+t_ternary_tree		*ternary_tree_new(char c)
+{
+	t_ternary_tree *new;
+
+	if (!(new = ft_memalloc(sizeof(t_ternary_tree))))
+		exit(1);
+	new->c = c;
+	return (new);
+}
+
+void			ternary_tree_print(t_ternary_tree *node)
+{
+	if (!node)
+		return ;
+	printf("c is %c\n", node->c);
+	if (node->match)
+		printf("match is %s\n", node->match);
+	if (node->left)
+		printf("\n left \n");
+	ternary_tree_print(node->left);
+	if (node->down)
+		printf("\n down \n");
+	ternary_tree_print(node->down);
+	//put_termcap("do");
+	if (node->right)
+		printf("\n right\n");
+	ternary_tree_print(node->right);
 }
 
 void		test_ternary_tree()
