7a905e3cef41b8cfede78d4f74f77e38b670abe3
ade-sede <adrien.de.sede@gmail.com>
2017-09-20 12:56:29 +0200

    Removed 2 lines in func_start that seemed to cause a leak with multiple stars in the line. Those lines were not used for anything

--- srcs/globing/star.c
+++ srcs/globing/star.c
@@ -2,16 +2,15 @@
 #include <sys/dir.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <stdio.h>
 
 int	func_star(t_matches *m, int to_match_i, int regex_i)
 {
 	int	bool_match;
 	int	i;
-	char	*to_match_cpy;
 
 	i = to_match_i;
 	bool_match = 0;
-	to_match_cpy = ft_strdup(m->to_match);
 	while (!bool_match && m->to_match[i])
 	{
 		bool_match || (bool_match |= match(m, i, regex_i + 1));
