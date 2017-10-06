61e6e3d4bf16dfaf7a8d5c03b829b5ddbacf5ad7
ade-sede <adrien.de.sede@gmail.com>
2017-10-06 11:26:25 +0200

    Edit_home and edit_end do not use right and left anymore

--- srcs/exec/exec_tree.c
+++ srcs/exec/exec_tree.c
@@ -31,17 +31,13 @@ int			exec_tree(t_ast *ast, t_lst_head *head)
 		else if (ast->symbol == COMPLEXE_COMMAND)
 		{
 			token = ast->token;
-			/* if (ft_strequ(token->value, "|")) */
 			if (token->id == TK_PIPE)
 				return (exec_pipe(ast, head));
 			else if (token->id == TK_SEMI)
-			/* else if (ft_strequ(token->value, ";")) */
 				return (semi_colon(ast, head));
 			else if (token->id == TK_AND_IF)
-			/* else if (ft_strequ(token->value, "&&")) */
 				return (logical_and(ast, head));
 			else if (token->id == TK_OR_IF)
-			/* else if (ft_strequ(token->value, "||")) */
 				return (logical_or(ast, head));
 		}
 	}
