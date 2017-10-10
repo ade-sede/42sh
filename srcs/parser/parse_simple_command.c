cd064c8602540aa67b4ca5ccedfa4db3c1b0da1d
ade-sede <adrien.de.sede@gmail.com>
2017-10-10 17:03:34 +0200

    It compiles

--- srcs/parser/parse_simple_command.c
+++ srcs/parser/parse_simple_command.c
@@ -55,9 +55,9 @@ static t_ast		*ast_create_node_from_word(t_token *token, t_list **token_list)
 		node = ast_create_node(token, NULL, CMD_NAME);
 	else
 	{
-		if (lex->cmd_name_open)
-			node = ast_create_node(token, NULL, CMD_PREFIX);
-		else
+		/* if (lex->cmd_name_open) */
+			/* node = ast_create_node(token, NULL, CMD_PREFIX); */
+		/* else */
 			node = ast_create_node(token, NULL, CMD_SUFFIX);
 	}
 	return (node);
