21f35d7c3a2a90dd314258204bf8e133afc69ffb
Thibaut DUMOULIN <tdumouli@e3r7p15.42.fr>
2017-11-24 20:58:30 +0100

    match

--- srcs/globing/match_open_dir.c
+++ srcs/globing/match_open_dir.c
@@ -18,6 +18,22 @@ int	end_match_open_dir(t_matches *m, char *cpy_to_match[2], char *m_dir_cpy, \
 	return (bool_match);
 }
 
+static void		cut(char *m_to_match, char **cpy_to_match, int *bool_match,
+		int thin)
+{
+	cpy_to_match[1] = m_to_match;
+	*bool_match |= thin;
+}
+
+static int		init(char **cpy_to_match, char **m_dir_cpy, char *dir_name,
+		t_matches *m)
+{
+	cpy_to_match[0] = m->to_match;
+	*m_dir_cpy = m->dir;
+	m->dir = dir_name;
+	return (0);
+}
+
 int	match_open_dir(t_matches *m, int m_i, int r_i, char *dir_name)
 {
 	int				bool_match;
@@ -26,34 +42,21 @@ int	match_open_dir(t_matches *m, int m_i, int r_i, char *dir_name)
 	char			*cpy_to_match[2];
 	struct dirent	*dirent;
 
-	dirent = NULL;
-	bool_match = 0;
-	cpy_to_match[0] = m->to_match;
 	if ((dir = opendir(dir_name)) == NULL)
 		return (bad_dir(dir_name));
-	m_dir_cpy = m->dir;
-	m->dir = dir_name;
+	bool_match = init(cpy_to_match, &m_dir_cpy, dir_name, m);
 	while ((dirent = readdir(dir)) != NULL)
 	{
 		if (ft_strequ(m->dir, "."))
-		{
-			m->to_match = ft_strdup(dirent->d_name);
-			cpy_to_match[1] = m->to_match;
-			bool_match |= (match(m, m_i, r_i));
-		}
+			cut((m->to_match = ft_strdup(dirent->d_name)), cpy_to_match,
+										&bool_match, (match(m, m_i, r_i)));
 		else if (ft_strequ(m->dir, "/"))
-		{
-			m->to_match = (ft_strjoin(m->dir, dirent->d_name));
-			cpy_to_match[1] = m->to_match;
-			bool_match |= (match(m, m_i + 1, r_i));
-		}
+			cut((m->to_match = (ft_strjoin(m->dir, dirent->d_name))),
+					cpy_to_match, &bool_match, (match(m, m_i + 1, r_i)));
 		else
-		{
-			m->to_match = (ft_strjoin3_free(m->dir, "/", dirent->d_name, 0));
-			cpy_to_match[1] = m->to_match;
-			if (dirent->d_name[0] != '.' || (dirent->d_name[0] == '.' && m->regex[r_i] == '.'))
-				bool_match |= (match(m, m_i + 1, r_i));
-		}
+			cut((m->to_match = (ft_strjoin3_free(m->dir, "/", dirent->d_name,
+0))), cpy_to_match, &bool_match, (*dirent->d_name != '.' || (*dirent->d_name ==
+		'.' && m->regex[r_i] == '.')) ? (match(m, m_i + 1, r_i)) : 0);
 		free(cpy_to_match[1]);
 	}
 	closedir(dir);
