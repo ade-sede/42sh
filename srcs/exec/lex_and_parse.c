d348cfc81ebe28e39fb4e6a16c2c0fbbee75f63f
Thibaut DUMOULIN <tdumouli@e3r7p15.42.fr>
2017-11-24 16:22:55 +0100

    lex_and_parser cut

--- srcs/exec/lex_and_parse.c
+++ srcs/exec/lex_and_parse.c
@@ -63,30 +63,43 @@ void	lex_reopen(t_list **token_list)
 			ft_simple_lst_create(reopen_token));
 }
 
+static t_list	*token_list(int res_l_and_p[0], t_lexer *lexer,
+			t_ast **ast, t_parser *parser)
+{
+	t_list		*token_list_ret;
+
+	token_list_ret = NULL;
+	res_l_and_p[0] = get_token_list(lexer, &token_list_ret,
+			singleton_env()->alias);
+	if (res_l_and_p[0] == LEXER_REOPEN)
+		lex_reopen(&token_list_ret);
+	res_l_and_p[1] = parse(parser, ast, token_list_ret);
+	return (token_list_ret);
+}
+
+static t_list	*init_sale(t_lexer *lexer, int *res, t_parser *parser, char *b)
+{
+	res[0] = -1;
+	res[1] = -1;
+	init_lexer(lexer, b);
+	init_parser(parser);
+	return (NULL);
+}
+
 int		lex_and_parse(t_ast *ast, char *buff, t_modes *modes)
 {
 	t_lexer		lexer;
-	t_list		*token_list;
-	t_list		*big_list;
-	int			res_lexer;
-	int			res_parser;
+	int			res_l_and_p[2];
 	t_parser	parser;
+	t_list		*big_list;
 
-	big_list = NULL;
-	res_lexer = -1;
-	res_parser = -1;
-	init_lexer(&lexer, buff);
-	init_parser(&parser);
-	while (!((res_lexer == LEXER_SUCCESS && res_parser == PARSER_SUCCESS) ||
-				res_parser == PARSER_ERROR))
+	big_list = init_sale(&lexer, res_l_and_p, &parser, buff);
+	while (!((res_l_and_p[0] == LEXER_SUCCESS &&
+	res_l_and_p[1] == PARSER_SUCCESS) || res_l_and_p[1] == PARSER_ERROR))
 	{
-		token_list = NULL;
-		res_lexer = get_token_list(&lexer, &token_list, singleton_env()->alias);
-		if (res_lexer == LEXER_REOPEN)
-			lex_reopen(&token_list);
-		res_parser = parse(&parser, &ast, token_list);
-		ft_simple_lst_pushback(&big_list, token_list);
-		if (res_lexer == LEXER_REOPEN || res_parser == PARSER_REOPEN)
+		ft_simple_lst_pushback(&big_list, token_list(res_l_and_p, &lexer, &ast,
+					&parser));
+		if (res_l_and_p[0] == LEXER_REOPEN || res_l_and_p[1] == PARSER_REOPEN)
 		{
 			if (!reopen(&lexer, &parser, modes))
 				return (0);
@@ -94,10 +107,10 @@ int		lex_and_parse(t_ast *ast, char *buff, t_modes *modes)
 				break ;
 		}
 	}
-	if (res_parser == PARSER_SUCCESS && !g_abort_opening)
+	if (res_l_and_p[1] == PARSER_SUCCESS && !g_abort_opening)
 		exec_main_loop(ast);
 	else
 		local_add_change_from_key_value(singleton_env(), "?", "1");
 	quit_lex_and_parse(&lexer, &parser, &big_list);
-	return ((res_parser == PARSER_ERROR && modes->mode > 0) ? 0 : 1);
+	return ((res_l_and_p[1] == PARSER_ERROR && modes->mode > 0) ? 0 : 1);
 }
